32ec293f93f621a7e9959d8caaacc14ab5fad9ad
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-07-02 10:25:48 +0530

    commented favicon error log

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
+    // icon: "/favicon.ico",
+    // shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
 }
